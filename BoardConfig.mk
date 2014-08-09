de53c378bb4d00da4776d5dcb1d1b30e02feace4
Jake Whatley <jacob.whatley91@gmail.com>
2014-08-09 19:10:15 -0400

    d803: update for kk source
    Change-Id: I64190a9f5871641ac2a3deae34873a555f7117ef

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,7 +16,7 @@
 
 include device/lge/g2-common/BoardConfigCommon.mk
 
-TARGET_KERNEL_CONFIG := custom_d803_defconfig
+TARGET_KERNEL_CONFIG := msm8974_d803_defconfig
 
 TARGET_OTA_ASSERT_DEVICE := d803,g2,galbi
 
